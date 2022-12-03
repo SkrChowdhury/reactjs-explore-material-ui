d8396c9d4a0b21043d07bcc0673fff529c1fb330
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-12-03 18:38:55 +0600

    button, loading button practice done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Button } from '@mui/material';
+import { useEffect, useState } from 'react';
+import LoadingButton from '@mui/lab/LoadingButton';
 function App() {
+  const [submitting, setSubmitting] = useState(false);
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setSubmitting(false);
+    }, 1000);
+    return () => {
+      clearTimeout(id);
+    };
+  }, [submitting]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+        style={{
+          background: '#f7f7f7',
+          width: '400px',
+          height: '400px',
+          borderRadius: '1rem',
+          padding: '2rem',
+        }}
+      >
+        <Button variant="contained" color="secondary" fullWidth sx={{ mb: 2 }}>
+          Checkout
+        </Button>
+
+        <LoadingButton
+          loading={submitting}
+          variant="outlined"
+          onClick={() => setSubmitting(true)}
         >
-          Learn React
-        </a>
-      </header>
+          Fetch data
+        </LoadingButton>
+      </div>
     </div>
   );
 }
